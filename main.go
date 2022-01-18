cf138d7f61cf7d9bccaf2c0ecb384ed74ad1ed94
Andrey Minin <ewkjf@mail.ru>
2022-01-18 23:47:54 +0300

    version2: added 1) support of mongoDB, 2) extracted code into multiple files and packages 3) added database storage usage

--- main.go
+++ main.go
@@ -1,101 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"log"
-	"math/rand"
+	"miniurl/handlers"
+	"miniurl/storage/mongostorage"
 	"net/http"
-	"strings"
-	"sync"
+	"os"
 	"time"
 )
-
-type HttpHandler struct {
-	storageMu sync.RWMutex
-	storage   map[string]string
-}
-
-func getRandomKey() string {
-	alphaBet := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-	rand.Shuffle(len(alphaBet), func(i, j int) {
-		alphaBet[i], alphaBet[j] = alphaBet[j], alphaBet[i]
-	})
-	id := string(alphaBet[:5])
-	return id
-}
-
-type PutRequestData struct {
-	Url string `json:"url"`
-}
-
-type PutResponseData struct {
-	Key string `json:"key"`
-}
-
-func (h *HttpHandler) handlePutUrl(w http.ResponseWriter, r *http.Request) {
-	var data PutRequestData
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	newUrlKey := getRandomKey()
-
-	h.storageMu.Lock()
-	h.storage[newUrlKey] = data.Url
-	h.storageMu.Unlock()
-
-	response := PutResponseData{
-		Key: newUrlKey,
-	}
-	rawResponse, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(rawResponse)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-}
-
-func (h *HttpHandler) handleGetUrl(w http.ResponseWriter, r *http.Request) {
-	key := strings.Trim(r.URL.Path, "/")
-	h.storageMu.RLock()
-	url, found := h.storage[key]
-	h.storageMu.RUnlock()
-
-	if !found {
-		http.NotFound(w, r)
-		return
-	}
-	http.Redirect(w, r, url, http.StatusPermanentRedirect)
-}
-
-func handleRoot(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Hello from Server!"))
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain")
-}
 
 func NewServer() *http.Server {
 	r := mux.NewRouter()
 
-	handler := &HttpHandler{
-		storage: make(map[string]string),
+	mongoUrl := os.Getenv("MONGO_URL")
+	mongoStorage := mongostorage.NewStorage(mongoUrl)
+	handler := &handlers.HttpHandler{
+		Storage: mongoStorage,
 	}
 
-	r.HandleFunc("/", handleRoot)
-	r.HandleFunc("/{shorturl:\\w{5}}", handler.handleGetUrl).Methods(http.MethodGet)
-	r.HandleFunc("/api/urls", handler.handlePutUrl).Methods(http.MethodPost)
+	r.HandleFunc("/", handlers.HandleRoot)
+	r.HandleFunc("/{shorturl:\\w{5}}", handler.HandleGetUrl).Methods(http.MethodGet)
+	r.HandleFunc("/api/urls", handler.HandlePutUrl).Methods(http.MethodPost)
 
 	return &http.Server{
 		Handler:      r,
