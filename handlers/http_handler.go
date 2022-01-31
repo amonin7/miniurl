b0e9489be12f7ccd0a584760685b7706eb54d3ae
Andrey Minin <ewkjf@mail.ru>
2022-01-31 22:25:11 +0300

    version3: added support of limiter, but this does not work, as expected

--- handlers/http_handler.go
+++ handlers/http_handler.go
@@ -3,15 +3,28 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"miniurl/ratelimit"
 	"miniurl/storage"
 	"net/http"
 	"strings"
 	"sync"
+	"time"
 )
+
+func NewHTTPHandler(storage storage.Storage, limiterFactory *ratelimit.Factory) *HttpHandler {
+	return &HttpHandler{
+		Storage:   storage,
+		postLimit: limiterFactory.NewLimiter("post_url", 10*time.Second, 2),
+		getLimit:  limiterFactory.NewLimiter("get_url", 1*time.Minute, 10),
+	}
+}
 
 type HttpHandler struct {
 	StorageMu sync.RWMutex
 	Storage   storage.Storage
+
+	postLimit *ratelimit.Limiter
+	getLimit  *ratelimit.Limiter
 }
 
 type PutRequestData struct {
@@ -23,8 +36,18 @@ type PutResponseData struct {
 }
 
 func (h *HttpHandler) HandlePutUrl(w http.ResponseWriter, r *http.Request) {
+	canDo, err := h.postLimit.CanDoAt(r.Context(), time.Now())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if !canDo {
+		http.Error(w, "rate limit exceeded", http.StatusTooManyRequests)
+		return
+	}
+
 	var data PutRequestData
-	err := json.NewDecoder(r.Body).Decode(&data)
+	err = json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -54,6 +77,16 @@ func (h *HttpHandler) HandlePutUrl(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *HttpHandler) HandleGetUrl(w http.ResponseWriter, r *http.Request) {
+	canDo, err := h.getLimit.CanDoAt(r.Context(), time.Now())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if !canDo {
+		http.Error(w, "rate limit exceeded", http.StatusTooManyRequests)
+		return
+	}
+
 	key := strings.Trim(r.URL.Path, "/")
 
 	url, err := h.Storage.GetURL(r.Context(), storage.URLKey(key))
